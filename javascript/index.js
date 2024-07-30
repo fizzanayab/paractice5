3ae198d3300b7758306ac8ee3eb9603ed56a9ee5
fizza nayab <tab2baku@gmail.com>
2024-07-30 22:57:54 +0400

    updated

--- javascript/index.js
+++ javascript/index.js
@@ -1,6 +1,29 @@
+
 $(".info-item .btn").click(function(){
-    $(".container").toggleClass("log-in");
-  });
-  $(".container-form .btn").click(function(){
-    $(".container").addClass("active");
-  })
+  $(".container").toggleClass("log-in");
+});
+
+// $(".container-form .btn").click(function(){
+//   $(".container").addClass("active");
+// })
+
+function login(){
+  let username = document.getElementById("username").value
+  let password = document.getElementById("password").value
+  let secret = document.getElementById("secret").value
+  
+  let username_db = "Fizza"
+  let password_db = "12345"
+  let secret_code = "Riocat"
+
+  if(username_db == username && password_db == password || secret_code == secret){
+   var element = document.getElementsByClassName("container")[0]; element.classList.add("active");
+
+  }else{
+    var element = document.getElementById("myDIV");
+    element.classList.toggle("mystyle"); 
+  
+    var element = document.getElementById("myDIV1");
+    element.classList.toggle("mystyle");  }
+  }
+    
